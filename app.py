dc4d4ac08b6b4d0aedea3eaae2ed1c0e34b86bcf
wz9314 <wxz317@case.edu>
2021-12-13 00:39:10 -0500

    user-group, event-group relations

--- app.py
+++ app.py
@@ -70,10 +70,27 @@ USER_ENDPOINT = 'https://m6p93ab8g4.execute-api.us-east-2.amazonaws.com/prod/use
 EVENT_ENDPOINT = 'https://m6p93ab8g4.execute-api.us-east-2.amazonaws.com/prod/event'
 GROUP_ENDPOINT = 'https://m6p93ab8g4.execute-api.us-east-2.amazonaws.com/prod/group'
 
+@app.route('/recommendedEvents/<user_id>', methods=["GET"])
+def recommendedEvents(user_id):
+    # All event ids that user joined
+    joinedEventIds = set(ast.literal_eval(requests.get(USER_ENDPOINT + '/getEvents/' + user_id).text))
+
+    # All group ids that user joined
+    joinedGroupIds = ast.literal_eval(requests.get(USER_ENDPOINT + '/getGroups/' + user_id).text)
+
+    recommendedEventList = []
+    for joinedGroupId in joinedGroupIds:
+        # All event ids inside the group that user joined
+        eventIdsInJoinedGroup = ast.literal_eval(requests.get(GROUP_ENDPOINT + '/getEvents/' + joinedGroupId).text)
+        for eventIdInJoinedGroup in eventIdsInJoinedGroup:
+            # if this event id inside (the group that user joined) is not joined by user, append the event info
+            if eventIdInJoinedGroup not in joinedEventIds:
+                recommendedEventList.append(requests.get(EVENT_ENDPOINT + '/event/' + eventIdInJoinedGroup).text)
+    return Response(json.dumps(recommendedEventList, default=list), status=200, content_type="application/json")
 
 @app.route('/getMyEvents', methods=["GET"])
-def getMyEvents():
-    eventIds = ast.literal_eval(requests.get(USER_ENDPOINT + '/getEvent/1').text)
+def registeredEvents():
+    eventIds = ast.literal_eval(requests.get(USER_ENDPOINT + '/getEvents/1').text)
 
     eventList = []
     for eventId in eventIds:
@@ -81,6 +98,7 @@ def getMyEvents():
         eventList.append(requests.get(EVENT_ENDPOINT + '/event/' + eventId).text)
 
     return Response(json.dumps(eventList, default=list), status=200, content_type="application/json")
+
 
 async def userAddEvent(user, event):
     res = requests.post(USER_ENDPOINT + '/addEvent/' + user + '/' + event).text
@@ -94,8 +112,7 @@ async def addUserEventRelation(user_id, event_id):
     await asyncio.gather(userAddEvent(user_id, event_id), eventAddUser(event_id, user_id))
 
 async def userRemoveEvent(user, event):
-    a = requests.delete(USER_ENDPOINT + '/removeEvent/' + user + '/' + event)
-    res = a.json()
+    res = requests.delete(USER_ENDPOINT + '/removeEvent/' + user + '/' + event).text
     return res
 
 async def eventRemoveUser(event, user):
@@ -114,6 +131,86 @@ def addUserEvent(user_id, event_id):
 def removeUserEvent(user_id, event_id):
     asyncio.run(removeUserEventRelation(user_id, event_id))
     return Response(json.dumps('Success', default=str), status=200, content_type="application/json")
+
+
+
+
+async def userAddGroup(user, group):
+    res = requests.post(USER_ENDPOINT + '/addGroup/' + user + '/' + group).text
+    return res
+
+async def groupAddUser(group, user):
+    res = requests.post(GROUP_ENDPOINT + '/addUser/' + group + '/' + user).text
+    return res
+
+async def addUserGroupRelation(user_id, group_id):
+    await asyncio.gather(userAddGroup(user_id, group_id), groupAddUser(group_id, user_id))
+
+async def userRemoveGroup(user, group):
+    res = requests.delete(USER_ENDPOINT + '/removeGroup/' + user + '/' + group).text
+    return res
+
+async def groupRemoveUser(group, user):
+    res = requests.delete(GROUP_ENDPOINT + '/removeUser/' + group + '/' + user).text
+    return res
+
+async def removeUserGroupRelation(user_id, group_id):
+    await asyncio.gather(userRemoveGroup(user_id, group_id), groupRemoveUser(group_id, user_id))
+
+@app.route('/addUserGroup/<user_id>/<group_id>', methods=["POST"])
+def addUserGroup(user_id, group_id):
+    asyncio.run(addUserGroupRelation(user_id, group_id))
+    return Response(json.dumps('Success', default=str), status=200, content_type="application/json")
+
+@app.route('/removeUserGroup/<user_id>/<group_id>', methods=["DELETE"])
+def removeUserGroup(user_id, group_id):
+    asyncio.run(removeUserGroupRelation(user_id, group_id))
+    return Response(json.dumps('Success', default=str), status=200, content_type="application/json")
+
+
+
+
+
+async def eventAddGroup(event, group):
+    res = requests.post(EVENT_ENDPOINT + '/addGroup/' + event + '/' + group).text
+    return res
+
+
+async def groupAddEvent(group, event):
+    res = requests.post(GROUP_ENDPOINT + '/addEvent/' + group + '/' + event).text
+    return res
+
+
+async def addEventGroupRelation(event_id, group_id):
+    await asyncio.gather(eventAddGroup(event_id, group_id), groupAddEvent(group_id, event_id))
+
+
+async def eventRemoveGroup(event, group):
+    res = requests.delete(EVENT_ENDPOINT + '/removeGroup/' + event + '/' + group).text
+    return res
+
+
+async def groupRemoveEvent(group, event):
+    res = requests.delete(GROUP_ENDPOINT + '/removeEvent/' + group + '/' + event).text
+    return res
+
+
+async def removeEventGroupRelation(event_id, group_id):
+    await asyncio.gather(eventRemoveGroup(event_id, group_id), groupRemoveEvent(group_id, event_id))
+
+
+@app.route('/addEventGroup/<event_id>/<group_id>', methods=["POST"])
+def addEventGroup(event_id, group_id):
+    asyncio.run(addEventGroupRelation(event_id, group_id))
+    return Response(json.dumps('Success', default=str), status=200, content_type="application/json")
+
+
+@app.route('/removeEventGroup/<event_id>/<group_id>', methods=["DELETE"])
+def removeEventGroup(event_id, group_id):
+    asyncio.run(removeEventGroupRelation(event_id, group_id))
+    return Response(json.dumps('Success', default=str), status=200, content_type="application/json")
+
+
 
 @app.route('/getEventDetails/<event_id>', methods=["GET"])
 def getEventDetail(event_id):
@@ -135,11 +232,11 @@ def creatNewEvent():
     type_id = request.json['type_id']
     venue_json = request.json['venue']
 		
-    res = json.loads(requests.post(EVENT_ENDPOINT + '/eventvenue', json=venue_json).content)
+    res = json.loads(requests.post(API_ENDPOINT + '/eventvenue', json=venue_json).content)
     new_venue_id = res['location'].split('/')[3]
 
     new_event_json = {'organizer_id': organizer_id, 'type_id': type_id, 'venue_id': new_venue_id, 'starttime': starttime, 'endtime': endtime, 'description': description}
-    result = requests.post(EVENT_ENDPOINT + '/event', json=new_event_json).text
+    result = requests.post(EVENT_ENDPOINT, json=new_event_json).text
     new_event_id=(json.loads(result)['location'].split('/'))[3]
 
     asyncio.run(addUserEventRelation(str(organizer_id), new_event_id))
